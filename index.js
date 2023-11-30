fedb7a2d03636ac088e08474ea9d4d73b4cbd833
Rahaf0B <rahafbakeer123@gmail.com>
2023-12-01 01:20:56 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs=require('fs/promises');
+const fileNames=require('./config.json');
+const Countable=require('countable')
+
+async function openFile(filename){
+   try{ 
+    const data=await fs.readFile(filename,'utf-8');
+     Countable.count(data.toString(), counter => console.log(filename+": "+counter.words+" words")); 
+} 
+catch(error) {
+            console.log("error occurred while reading the file: "+filename);     
+}
+}
+
+for (const file of fileNames["files"]) {
+    openFile(file);
+  }
