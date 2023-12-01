9237360706c5aa0187b1321fc4b471be85d08f72
Rahaf0B <rahafbakeer123@gmail.com>
2023-12-01 15:15:30 +0200

    changes

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const fs=require('fs/promises');
 const fileNames=require('./config.json');
 const Countable=require('countable')
 
+
 async function openFile(filename){
    try{ 
     const data=await fs.readFile(filename,'utf-8');
-     Countable.count(data.toString(), counter => console.log(filename+": "+counter.words+" words")); 
+     Countable.count(data, counter => console.log(filename+": "+counter.words+" words")); 
 } 
 catch(error) {
             console.log("error occurred while reading the file: "+filename);     
